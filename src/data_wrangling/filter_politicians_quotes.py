c3c1042b658d7116b1624de02334ab0d4de96959
Alexis <alexisjair-98@hotmail.com>
2021-12-17 17:44:10 +0100

    Milestone 3 notebook

--- src/data_wrangling/filter_politicians_quotes.py
+++ src/data_wrangling/filter_politicians_quotes.py
@@ -2,6 +2,21 @@ import pandas as pd
 import pywikibot
 import os
 
+def filter_quotes(valid_speakers, quotes_dir, out_dir):
+    # Loop through the 5 quotebank datasets
+    for file in os.listdir(quotes_dir):
+        file_out = out_dir + os.path.splitext(os.path.splitext(file)[0])[0] + '-politicians.csv'
+        with pd.read_json(os.path.join(quotes_dir, file), lines=True, compression='bz2', chunksize=10000) as df_reader:
+            # Loop through each of the chunks of the dataset
+            for index, chunk in enumerate(df_reader):
+                chunk['qid'] = chunk['qids'].apply(lambda x: x[0] if len(x) > 0 else None)
+                # Remove the quotes not belonging to politicians
+                idx_to_drop = chunk.index[~chunk.qid.isin(valid_speakers)]
+                chunk.drop(idx_to_drop, inplace=True)
+                # Append to file
+                chunk.to_csv(file_out, mode=('a' if index else 'w'),
+                             index=False, compression="gzip", header=(index == 0))
+
 if __name__ == "__main__":
     data_dir = '../../data/'
     # Get all the speakers that have a party in the parquet file
